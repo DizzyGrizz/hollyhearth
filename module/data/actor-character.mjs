5bb5d4a718e884f30e4748f6d5457279ae1a5b07
DizzyGrizz <jascomo95@gmail.com>
2026-04-24 13:29:13 -0400

    Customize actor data

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -13,10 +13,18 @@ export default class HollyHearthCharacter extends HollyHearthActorBase {
       }),
     });
 
-    // Iterate over ability names and create a new SchemaField for each.
-    schema.abilities = new fields.SchemaField(Object.keys(CONFIG.HOLLY_HEARTH.abilities).reduce((obj, ability) => {
-      obj[ability] = new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
+    // Iterate over approach names and create a new SchemaField for each.
+    schema.approaches = new fields.SchemaField(Object.keys(CONFIG.HOLLY_HEARTH.approaches).reduce((obj, approach) => {
+      obj[approach] = new fields.SchemaField({
+        value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0, max: 2 }),
+      });
+      return obj;
+    }, {}));
+
+    // Iterate over skill names and create a new SchemaField for each.
+    schema.skills = new fields.SchemaField(Object.keys(CONFIG.HOLLY_HEARTH.skills).reduce((obj, skill) => {
+      obj[skill] = new fields.SchemaField({
+        value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0, max: 3 }),
       });
       return obj;
     }, {}));
@@ -26,26 +34,38 @@ export default class HollyHearthCharacter extends HollyHearthActorBase {
 
   prepareDerivedData() {
     // Loop through ability scores, and add their modifiers to our sheet output.
-    for (const key in this.abilities) {
-      // Calculate the modifier using d20 rules.
-      this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
-      // Handle ability label localization.
-      this.abilities[key].label = game.i18n.localize(CONFIG.HOLLY_HEARTH.abilities[key]) ?? key;
-    }
+    //for (const key in this.abilities) {
+    //  // Handle ability label localization.
+    //  this.abilities[key].label = game.i18n.localize(CONFIG.HOLLY_HEARTH.abilities[key]) ?? key;
+    //}
   }
 
-  getRollData() {
+  getCheckData() {
     const data = {};
+    data.appr = this.getApproachData();
+    data.skill = this.getSkillData();
 
-    // Copy the ability scores to the top level, so that rolls can use
-    // formulas like `@str.mod + 4`.
-    if (this.abilities) {
-      for (let [k,v] of Object.entries(this.abilities)) {
+    return data
+  }
+
+  getApproachData() {
+    const data = {}
+    if (this.approaches) {
+      for (let [k,v] of Object.entries(this.approaches)) {
         data[k] = foundry.utils.deepClone(v);
       }
     }
 
-    data.lvl = this.attributes.level.value;
+    return data
+  }
+
+  getSkillData() {
+    const data = {}
+    if (this.skills) {
+      for (let [k,v] of Object.entries(this.skills)) {
+        data[k] = foundry.utils.deepClone(v);
+      }
+    }
 
     return data
   }
--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -7,15 +7,7 @@ export default class HollyHearthActorBase extends HollyHearthDataModel {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = {};
 
-    schema.health = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 10 })
-    });
-    schema.power = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 5 })
-    });
-    schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
+    schema.biography = new fields.HtmlField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
     return schema;
   }
