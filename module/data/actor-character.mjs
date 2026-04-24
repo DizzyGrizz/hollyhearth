4c1460ed2866eb6f5f0284fff158f25801122169
DizzyGrizz <jascomo95@gmail.com>
2026-04-24 13:48:45 -0400

    Try to get characters set up

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -7,12 +7,6 @@ export default class HollyHearthCharacter extends HollyHearthActorBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.attributes = new fields.SchemaField({
-      level: new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 1 })
-      }),
-    });
-
     // Iterate over approach names and create a new SchemaField for each.
     schema.approaches = new fields.SchemaField(Object.keys(CONFIG.HOLLY_HEARTH.approaches).reduce((obj, approach) => {
       obj[approach] = new fields.SchemaField({
